4518b551d5e1f6412a314d418e03cfbe94ad2a2a
Shabbir Saifee <shabbirsaifee91@gmail.com>
2025-05-29 11:12:34 -0400

    add another module

---
+++ terraform/other-module/main.tf
@@ -0,0 +1,9 @@
+resource "tfe_team" "platform_admin" {
+  name         = "other module"
+  organization = "other module"
+  organization_access {
+    read_projects   = true
+    read_workspaces = true
+  }
+  visibility = "organization"
+}
